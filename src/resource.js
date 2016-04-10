1abd314bbef156bba23b387be85890746441de8c
scm96steven <scm96steven@gmail.com>
2016-04-10 18:01:40 -0400

    Agregados los sprites a Resource.js
    Se agregaron a resource los sprites que estan hasta el momento

--- src/resource.js
+++ src/resource.js
@@ -1,5 +1,10 @@
 var res = {
     HelloWorld_png : "res/HelloWorld.png",
+    Scoring_png: "scoring.png",
+    Pigs_png: "sprite_INGAME.png",
+    Pigs2_png: "sprite1.png",
+    Transparency_png: "transparency.png", 
+    
 };
 
 var g_resources = [];
