c9391c4797df8af4f587ce5f7f8d95282905d336
adalbertofc <cleto321>
2016-04-12 18:08:28 -0400

    button working
    usando ccui button

--- src/MenuGameScene.js
+++ src/MenuGameScene.js
@@ -1,31 +1,29 @@
 (function(){
     var MenuGameScene = cc.Scene.extend({
         backgroundLayer: null,
-        level: null,
+        button:new ccui.Button(),
         cuadro: null,
         ctor: function(){
             this._super();
             this.scheduleUpdate();
             
             this.background=new abird.BackgroundLayer(res.menuBack);
-            this.level= new cc.Sprite(res.play);
-            this.level.setPosition(cc.winSize.width / 2,cc.winSize.height / 2);
             
             this.addChild(this.background,0);
-            this.addChild(this.level,1);
-            this.cuadro=this.level.getBoundingBox;
-            cc.eventManager.addListener({
-            event: cc.EventListener.TOUCH_ONE_BY_ONE,
-            onTouchBegan: function(touch, event) {
-                
-                //if(cc.rectContainsPoint(this.cuadro,touch.getLocation))
-                    cc.director.runScene(new abird.MainGameScene());
             
-            },
-			
-			
-		}, this);
+            
+            this.button.loadTextures(res.play,res.play);
+            this.button.setAnchorPoint(cc.p(0,0));
+            this.button.x=cc.winSize.width/2;
+            this.button.y=cc.winSize.height/2;
+            this.addChild(this.button);
         
+            this.button.addTouchEventListener(this.touchEvent, this);
+            this.button.addClickEventListener(function () {
+                cc.director.runScene(new abird.MainGameScene());
+            }, this); 
+            
+            
             return true;
         },
         
